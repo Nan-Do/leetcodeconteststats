3ff5013ab09d7fb440e719bb85a0c1e2a4497428
Nando <tarin.f@gmail.com>
2026-03-09 14:34:34 +0900

    Modified the backend to show the number of wins each user has over the other

--- routes/api.js
+++ routes/api.js
@@ -53,9 +53,32 @@ router.get('/compare', (req, res) => {
   const region1 = r1 || 'US';
   const region2 = r2 || 'US';
   try {
+    const u1_history = getUserHistory(u1, region1);
+    const u1_stats = getUserStats(u1, region1);
+    const u2_history = getUserHistory(u2, region2);
+    const u2_stats = getUserStats(u2, region2);
+
+    const u1_results = new Map();
+    u1_history.forEach((contest) => u1_results.set(contest["contest_slug"], contest["rank"]))
+    const u2_results = new Map();
+    u2_history.forEach((contest) => u2_results.set(contest["contest_slug"], contest["rank"]))
+
+    let u1_victories = 0;
+    for (const [contest_slug, rank] of u1_results) {
+      if (u2_results.has(contest_slug) && rank < u2_results.get(contest_slug)) u1_victories += 1;
+    }
+    let u2_victories = 0;
+    for (const [contest_slug, rank] of u2_results) {
+      if (u1_results.has(contest_slug) && rank < u1_results.get(contest_slug)) u2_victories += 1;
+    }
+
+    console.log(`${u1} has ${u1_victories} wins over ${u2}`);
+    console.log(`${u2} has ${u2_victories} wins over ${u1}`);
+
     res.json({
-      user1: { history: getUserHistory(u1, region1), stats: getUserStats(u1, region1) },
-      user2: { history: getUserHistory(u2, region2), stats: getUserStats(u2, region2) },
+      user1: { history: u1_history, stats: u1_stats },
+      user2: { history: u2_history, stats: u2_stats },
+      wins: { user1: u1_victories, user2: u2_victories },
     });
   } catch (err) {
     res.status(500).json({ error: err.message });

2cb5a85e28d986a2d13c816a549a2ba41f8b5acd
Nando <tarin.f@gmail.com>
2026-03-10 17:58:03 +0900

    Updated sql commands to prepare the database file for deployment

--- thin_db.sql
+++ thin_db.sql
@@ -4,7 +4,6 @@ ALTER TABLE contest_results RENAME TO contest_results_old;
 -- Step 2: Create new table with only the required fields
 CREATE TABLE contest_results (
     contest_id INTEGER,
-    username TEXT NOT NULL,
     user_slug TEXT,
     rank INTEGER NOT NULL,
     score INTEGER NOT NULL,
@@ -13,8 +12,28 @@ CREATE TABLE contest_results (
 );
 
 -- Step 3: Copy data
-INSERT INTO contest_results (contest_id, username, user_slug, rank, score, data_region)
-SELECT contest_id, username, user_slug, rank, score, data_region FROM contest_results_old;
+INSERT INTO contest_results (contest_id, user_slug, rank, score, data_region)
+SELECT contest_id, user_slug, rank, score, data_region FROM contest_results_old;
 
 -- Step 4: Drop the old table
 DROP TABLE contest_results_old;
+
+-- Step 5: Generate the indices for the searches
+CREATE INDEX idx_user_stats_history ON contest_results(
+    user_slug,
+    data_region,
+    contest_id,
+    rank,
+    score
+);
+
+CREATE INDEX idx_user_search ON contest_results(
+    user_slug COLLATE NOCASE, 
+    data_region
+);
+
+-- Step 6: Make sure the database is ready to be uploaded to Turso
+PRAGMA journal_mode = WAL;
+
+-- Step 7: Optimize the space
+VACUUM;
